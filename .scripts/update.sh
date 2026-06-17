d8dccd111cee5268d4f1b3af720b1355bb72c12c
7erry <w.7erry@gmail.com>
2026-06-17 09:39:20 +0100

    CI: FIx CI shell command

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -2,20 +2,20 @@
 #echo "Check/Update theme version"
 #npm install hexo-theme-redefine@latest
 echo "Refresh hexo blog system"
-hexo clean
+npx hexo clean
 # 删除追番数据
-# hexo bangumi -d
+# npx hexo bangumi -d
 # 删除追剧数据
-# hexo cinema -d
+# npx hexo cinema -d
 # 删除游戏数据
-# hexo game -d
-hexo generate 
+# npx hexo game -d
+npx hexo generate
 # 更新追番数据
-hexo bangumi -u
+npx hexo bangumi -u
 # 使用 bili 源时显示追番进度（需要 SESSDATA）
-# hexo bangumi -u 'your_sessdata_here'
+# npx hexo bangumi -u 'your_sessdata_here'
 # 更新追剧数据
-hexo cinema -u
+npx hexo cinema -u
 # 更新游戏数据
-hexo game -u
-hexo deploy
+npx hexo game -u
+npx hexo deploy
